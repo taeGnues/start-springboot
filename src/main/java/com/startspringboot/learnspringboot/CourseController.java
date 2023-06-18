9f41f6da5b9d889dc760ba7cd70c527cf1c8b91b
정태승 <hi563@g.skku.edu>
2023-06-18 18:39:06 +0900

    First Spring Boot

---
+++ src/main/java/com/startspringboot/learnspringboot/CourseController.java
@@ -0,0 +1,20 @@
+package com.startspringboot.learnspringboot;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class CourseController {
+// course id, name, author
+    @RequestMapping(value="/courses")
+    public List<Course> retrieveAllCourses(){
+        return Arrays.asList(
+                new Course(1, "Learn AWS", "in28minutes"),
+                new Course(2, "Learn DevOps", "in28minutes")
+        );
+    }
+}
