4281833a9b12d760de169b2a0a9bdfb9a01edd05
정태승 <hi563@g.skku.edu>
2023-06-19 18:26:03 +0900

    Spring boot 프로덕션 환경 준비 - profile

--- src/main/java/com/startspringboot/learnspringboot/CourseController.java
+++ src/main/java/com/startspringboot/learnspringboot/CourseController.java
@@ -14,7 +14,8 @@ public class CourseController {
     public List<Course> retrieveAllCourses(){
         return Arrays.asList(
                 new Course(1, "Learn AWS", "in28minutes"),
-                new Course(2, "Learn DevOps", "in28minutes")
+                new Course(2, "Learn DevOps", "in28minutes"),
+                new Course(3, "Learn DevOps", "in28minutes")
         );
     }
 }
